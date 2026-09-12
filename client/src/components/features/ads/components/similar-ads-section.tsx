e727dbd82ee64d0010c79a7954e6c3f094092744
AdamMaster <lampezhev86@gmail.com>
2026-09-12 13:24:23 +0300

    коммит ad-card

--- client/src/components/features/ads/components/similar-ads-section.tsx
+++ client/src/components/features/ads/components/similar-ads-section.tsx
@@ -15,7 +15,7 @@ export function SimilarAdsSection({ ads }: SimilarAdsSectionProps) {
       </Heading>
       <div className='flex snap-x snap-mandatory [scrollbar-width:none] gap-3 overflow-x-auto overscroll-x-contain pb-1 sm:gap-4 [&::-webkit-scrollbar]:hidden'>
         {ads.map(ad => (
-          <div key={ad.id} className='w-36 flex-shrink-0 snap-start sm:w-[260.8px]'>
+          <div key={ad.id} className='w-36 w-[196px] flex-shrink-0 snap-start lg:w-[238.5px] xl:w-[260.8px]'>
             <AdCard ad={ad} />
           </div>
         ))}
