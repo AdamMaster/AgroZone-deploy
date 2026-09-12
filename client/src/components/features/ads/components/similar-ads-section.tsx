b6b7ee6e1287976046ed64f52421a5aa2cd5d583
AdamMaster <lampezhev86@gmail.com>
2026-09-12 13:27:43 +0300

    коммит gap

--- client/src/components/features/ads/components/similar-ads-section.tsx
+++ client/src/components/features/ads/components/similar-ads-section.tsx
@@ -13,7 +13,7 @@ export function SimilarAdsSection({ ads }: SimilarAdsSectionProps) {
       <Heading level={3} className='mb-3'>
         Похожие объявления
       </Heading>
-      <div className='flex snap-x snap-mandatory [scrollbar-width:none] gap-3 overflow-x-auto overscroll-x-contain pb-1 sm:gap-4 [&::-webkit-scrollbar]:hidden'>
+      <div className='flex snap-x snap-mandatory [scrollbar-width:none] gap-x-6 gap-y-4 overflow-x-auto overscroll-x-contain pb-1 sm:gap-4 sm:gap-x-2.5 md:gap-x-2.5 xl:gap-x-6 [&::-webkit-scrollbar]:hidden'>
         {ads.map(ad => (
           <div key={ad.id} className='w-36 w-[196px] flex-shrink-0 snap-start lg:w-[238.5px] xl:w-[260.8px]'>
             <AdCard ad={ad} />
